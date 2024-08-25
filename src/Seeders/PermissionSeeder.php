d7f29325d86d46f262a1248bdc84ccf79e1b9baa
Burhan Ibrahimi <burhanibrahimi@orcaya.com>
2024-08-25 06:11:16 +0200

    remove laravel dependencies versions

--- src/Seeders/PermissionSeeder.php
+++ src/Seeders/PermissionSeeder.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Seeder;
 use Bi\Users\Exception\BiException;
 use Illuminate\Support\Facades\Log;
 use Bi\Users\Interfaces\RoleInterface;
-use Spatie\Permission\Models\Permission;
 use Spatie\Permission\PermissionRegistrar;
+use Bi\Users\Interfaces\PermissionInterface;
 use Spatie\Permission\Models\Role as SpatieRole;
 use Bi\Users\Interfaces\RolePermissionsInterface;
+use Spatie\Permission\Models\Permission as SpatiePermission;
 
 class PermissionSeeder extends Seeder
 {
@@ -25,12 +26,14 @@ class PermissionSeeder extends Seeder
 
         /** @var SpatieRole $role */
         foreach ($roles as $role) {
+
             $connection = $this->getLinked($role);
 
             $this->doConnect($role, $connection);
         }
     }
 
+    /** @return SpatieRole[] */
     private function createRoles(): array
     {
         $roles = config('bi-users.rbac.roles');
@@ -61,6 +64,7 @@ class PermissionSeeder extends Seeder
         return $list;
     }
 
+    /** @return array<string, SpatiePermission> */
     private function createPermissions(): array
     {
         $permissions = config('bi-users.rbac.permissions');
@@ -85,13 +89,14 @@ class PermissionSeeder extends Seeder
                 throw new BiException('Invalid permission name');
             }
 
-            $list[$name] = Permission::findOrCreate($name);
+            $list[$name] = SpatiePermission::findOrCreate($name);
         }
 
         return $list;
     }
 
-    private function getLinked(SpatieRole $role)
+    /** @return iterable<int, PermissionInterface> */
+    private function getLinked(SpatieRole $role): iterable
     {
         /** @var RoleInterface[] $roles */
         $roles = config('bi-users.rbac.roles');
@@ -118,28 +123,35 @@ class PermissionSeeder extends Seeder
         return $permissions ?? [];
     }
 
+    /** @param PermissionInterface[] $permissions */
     private function doConnect(SpatieRole $role, iterable $permissions)
     {
+        $this->command->info('ROLE: ' . $role->name);
+
         try {
             foreach ($permissions as $i => $permission) {
                 $permissionObject = null;
 
                 if (is_string($permission)) {
-                    $permissionObject = Permission::findByName($permission);
+                    $permissionObject = SpatiePermission::findByName($permission);
                 }
 
                 if (my_is_enum($permission)) {
                     $permissionObject = $permission->getObject();
                 }
 
-                if (!is_a($permissionObject, Permission::class)) {
+                if (!is_a($permissionObject, SpatiePermission::class)) {
                     throw new BiException('Invalid permission name');
                 }
+
+                $this->command->info('  - PERMISSION: ' . $permissionObject->name);
 
                 $role->givePermissionTo($permissionObject);
             }
         } catch (Exception $e) {
             Log::critical($e->getMessage());
+
+            $this->command->error('  - ERROR: ' . $e->getMessage());
         }
     }
 
@@ -152,7 +164,7 @@ class PermissionSeeder extends Seeder
                 $name = isset($permission->value) ? $permission->value : $permission->key;
             }
 
-            $list[$name] = Permission::findOrCreate($name);
+            $list[$name] = SpatiePermission::findOrCreate($name);
         }
     }
 }
